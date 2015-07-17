72ac6ee1ff766fd5672eabcd1c71be89a2e87865
Simon Zelazny <simon@manisola.net>
2015-07-17 16:04:43 +0200

    Remove whitespace and group module declarations

--- src/x_message.erl
+++ src/x_message.erl
@@ -1,20 +1,16 @@
 -module(x_message).
+-export([new/3, is_message/1]).
 
 -behaviour(x_Addressable).
 -export([from/1, to/1]).
 
--export([new/3, is_message/1]).
-
 -export_type([x_message/0]).
-
--record(x_message, {from :: x_jid:jid(), 
-                    to :: x_jid:jid()}). 
-
--type x_message() :: #x_message{}.  
+-record(x_message, {from :: x_jid:jid(),
+                    to :: x_jid:jid()}).
+-type x_message() :: #x_message{}.
 
 -spec new(x_jid:jid(), x_jid:jid(), iolist()) -> x_message().
-new(From, To, _Body) ->
-    #x_message{from = From, to = To}.
+new(From, To, _Body) -> #x_message{from = From, to = To}.
 
 -spec is_message(any()) -> boolean().
 is_message(#x_message{}) -> true;
@@ -25,6 +21,3 @@ from(#x_message{from = From}) -> From.
 
 -spec to(x_message()) -> x_jid:jid().
 to(#x_message{to = To}) -> To.
-
-
-
